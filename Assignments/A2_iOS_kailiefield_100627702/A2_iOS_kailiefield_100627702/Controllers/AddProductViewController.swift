ef990f60a82e2b7a85edbd372020f4e11b0aa783
Kailie Field <kailie.field@gmail.com>
2025-03-30 12:00:27 -0400

    updating AddProductViewController to handle IBOutlets and IBAction --- to connect to storyboard

--- Assignments/A2_iOS_kailiefield_100627702/A2_iOS_kailiefield_100627702/Controllers/AddProductViewController.swift
+++ Assignments/A2_iOS_kailiefield_100627702/A2_iOS_kailiefield_100627702/Controllers/AddProductViewController.swift
@@ -1,29 +1,40 @@
-//
-//  AddProductViewController.swift
-//  A2_iOS_kailiefield_100627702
-//
-//  Created by Kailie Field on 2025-03-30.
-//
-
 import UIKit
+import CoreData
 
 class AddProductViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var providerField: UITextField!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton){
+        let newProduct = Product(context: context)
+        newProduct.id = UUID()
+        newProduct.name = nameField.text
+        newProduct.prodDesc = descriptionField.text
+        newProduct.price = Double(priceField.text ?? "") ?? 0.0
+        newProduct.provider = providerField.text
+        
+        do {
+            try context.save()
+            
+            navigationController?.popViewController(animated: true)
+        } catch {
+            
+            print("Error: \(error)")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
